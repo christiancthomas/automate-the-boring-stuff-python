5f5f907ae4239b501c8a7e9fc68096b13c2c0154
Christian Thomas <76794573+christiancthomas@users.noreply.github.com>
2020-12-30 19:16:47 -0600

    Add files via upload

---
+++ intro_to_python_part1.py
@@ -0,0 +1,6 @@
+myName = input('What is your name? ')
+print('Hello, ' + myName + '! My name is HAL.')
+myAge = input('How old are you? ')
+print('Cool! So your name is ' + myName + ' and you are ' + myAge + ' years old.')
+newAge = int(myAge) + 5
+print(myName + ', did you know in 5 years you will be ' + str(newAge) + '?')
